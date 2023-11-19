f222396ae723420d3cf3c2b59e2dc102317766e0
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-18 22:45:35 -0600

    trying to add dynamnic text to my homepage of the portfolio, on the title specifically

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+const portfolioDescriptions = ['Full Stack', 'React', 'Web Development'];
+
+function genRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+  }
+
+
 function Header() {
+    const keyWords = portfolioDescriptions[genRandomInt(2)]
+
     return (
         <header>
             <img src="src/assets/portfolio-pic.jpg" alt="Jack Stendeback in a suit." />
-            <h1>React Portfolio</h1>
+            <h1>My {keyWords} Portfolio</h1>
             <p>
                 An in depth look into my life as a Full Stack Web Developer!
             </p>
