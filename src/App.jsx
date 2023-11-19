ec53b2c0a53a9b2271db9a3e7fba5331d787976d
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-18 22:42:40 -0600

    adding first bits to app.jsx, to get a baseline front page up and running

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+function Header() {
+    return (
+        <header>
+            <img src="src/assets/portfolio-pic.jpg" alt="Jack Stendeback in a suit." />
+            <h1>React Portfolio</h1>
+            <p>
+                An in depth look into my life as a Full Stack Web Developer!
+            </p>
+        </header>
+    );
+}
+
 function Portfolio() {
     return (
         <div>
-            
-
+          <main>
+            <h2>Time to get started!</h2>
+          </main>
         </div>
     );
 }
